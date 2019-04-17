8e63cb2b643d0d33ae2d856ab105d04dcfaebdba
kemery <kemery@aperturecvo.com>
2019-04-17 15:16:48 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		class Hello
+		{
+			static void Main()
+			{
+				Console.WriteLine("HI Kim!");
+
+				// Keep the console window open in debug mode.
+				Console.WriteLine("Press any key to exit.");
+				Console.ReadKey();
+			}
+		}
+	}
+}
